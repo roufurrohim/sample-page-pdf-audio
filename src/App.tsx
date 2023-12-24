d00a4f26ed29db5b629072f2c1c4885ac7f7ed08
barru exclolab <barruexclolab@gmail.com>
2023-12-25 00:46:06 +0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import { useCallback, useState } from 'react';
+// Audio
+import { AudioPlayer } from 'react-audio-play';
+import Audio from "./assets/audio.mp3";
+// Pdf
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import PdfFile from "./assets/dummies.pdf";
+
+import type { PDFDocumentProxy } from 'pdfjs-dist';
+import { useResizeObserver } from '@wojtekmaj/react-hooks';
+
+import './App.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
+const options = {
+  cMapUrl: '/cmaps/',
+  standardFontDataUrl: '/standard_fonts/',
+};
+
+const resizeObserverOptions = {};
+
+const maxWidth = 800;
+
+type PDFFile = string | File | null;
+
+function App() {
+
+  const [file, setFile] = useState<PDFFile>(PdfFile);
+  const [numPages, setNumPages] = useState<number>();
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  function onFileChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const { files } = event.target;
+
+    if (files && files[0]) {
+      setFile(files[0] || null);
+    }
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
+
+  return (
+    <>
+        <header>
+          <h1>sample page</h1>
+        </header>
+        <div>
+          <AudioPlayer src={Audio} />
+        </div>
+        <div className="Example__container__load">
+          <label htmlFor="file">Load from file:</label>{' '}
+          <input onChange={onFileChange} type="file" />
+        </div>
+        <div className="Example__container__document" ref={setContainerRef}>
+          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+            {Array.from(new Array(numPages), (_el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+              />
+            ))}
+          </Document>
+        </div>
+    </>
+  )
+}
+
+export default App
